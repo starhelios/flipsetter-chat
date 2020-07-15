ed66b9be6de247da9d16f03c7df8ca12315ad62d
starhelios <starhelios94@gmail.com>
2020-07-15 08:41:13 +0300

    Change position of the personal video feed

--- src/components/WebRTC.js
+++ src/components/WebRTC.js
@@ -476,10 +476,10 @@ const styles = StyleSheet.create({
 
     },
     localVideo: {
-        height: 200,
-        width:100,
-        bottom:125,
-        left: config.layout.window.width/2-50,
+        height: 250,
+        width: 125,
+        bottom: 125,
+        right: 25,
         position:"absolute",
         zIndex:1,
 
