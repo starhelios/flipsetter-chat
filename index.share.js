0c82e0cd3238d331b00ba8ce8b5c8dd531307d49
starhelios <starhelios94@gmail.com>
2020-10-18 15:38:31 -0400

    remove  some files

--- index.share.js
+++
@@ -1,8 +0,0 @@
-/**
- * @format
- */
-import './src/config/ReactotronConfig';
-import { AppRegistry } from 'react-native';
-import Share from './src/screens/ShareScreen';
-
-AppRegistry.registerComponent('ShareMenuModuleComponent', () => Share);
